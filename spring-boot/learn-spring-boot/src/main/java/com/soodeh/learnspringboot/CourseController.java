215bffd7101939b1339e2f1507cf793ab1b56aec
Soudeh Nilforoushan <nilforoushan.soodeh@gmail.com>
2023-07-21 11:19:03 -0400

    Add files via upload

---
+++ spring-boot/learn-spring-boot/src/main/java/com/soodeh/learnspringboot/CourseController.java
@@ -0,0 +1,25 @@
+package com.soodeh.learnspringboot;
+//in course controller we wanto return the json files when user click on api
+
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseController {
+
+    @RequestMapping("/courses")
+    public List<Course> retrieveAllCourses() {
+        return Arrays.asList(
+                new Course(1, "Learn AWS", "in28minutes"),
+                new Course(2, "Learn DevOps", "in28minutes"),
+                new Course(3, "Learn Azure", "in28minutes"),
+                new Course(4, "Learningx AI", "in28minutes")
+
+        );
+    }
+
+}
---
+++ spring-boot/learn-spring-boot/src/main/java/com/soodeh/learnspringboot/CurrencyConfigurationController.java
@@ -0,0 +1,23 @@
+package com.soodeh.learnspringboot;
+//in course controller we wanto return the json files when user click on api
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CurrencyConfigurationController {
+
+    @Autowired
+    private CurrencyServiceConfiguration configuration;
+
+    @RequestMapping("/currency-configuration")
+    public CurrencyServiceConfiguration retrieveAllCourses() {
+        return configuration;
+    }
+
+}
